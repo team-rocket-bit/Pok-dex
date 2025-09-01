3bd63b4cd7459b274cafcdb22a70a3834c323798
unknown <Janay@nopalipolwerd.com>
2025-09-01 14:19:02 +0200

    am

---
+++ Db/pokedex.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS pokedex;
+
+CREATE DATABASE pokedex;
+
+CREATE TABLE pokemon (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Species VARCHAR(100) NOT NULL,
+    Height DECIMAL(5,2) NOT NULL,
+    Weight DECIMAL(5,2) NOT NULL,
+    Primary_ability VARCHAR(100) NOT NULL,
+    Hidden_ability VARCHAR(100),
+    Primary_type_id INT NOT NULL,
+    Secondary_type INT,
+    Habitat_id INT NOT NULL
+);
+
+INSERT INTO pokemon (
+    Name, Species, Height, Weight, Primary_ability, Hidden_ability, Primary_type_id, Secondary_type, Habitat_id
+)
+VALUES (
+    'Pikachu', 'Mouse Pokémon', 0.40, 6.00, 'Static', 'Lightning Rod', 13, NULL, 3
+);
+
+
+
