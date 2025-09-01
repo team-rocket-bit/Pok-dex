62d995afe84a08b86b8def7dc50a767ae9326e70
unknown <Janay@nopalipolwerd.com>
2025-09-01 16:09:52 +0200

    added

---
+++ PokeData/Data fetch.php
@@ -0,0 +1,24 @@
+
+<?php
+
+$apiUrl = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+
+$response = file_get_contents($apiUrl);
+if ($response === FALSE) {
+    die("Error occurred while fetching API data.");
+}
+$data = json_decode($response, true);
+if ($data === null) {
+    die("Error decoding JSON data.");
+}
+$fileSaved = file_put_contents('data.json', json_encode($data, JSON_PRETTY_PRINT));
+if ($fileSaved === false) {
+    die("Error saving data to file.");
+}
+
+echo "Data fetched and saved successfully.";
+?>
+
+
+
